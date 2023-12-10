7a50fb799257b6bfde810474210b22ccd6cdb225
Dre Taylor <drtaylor.gt@gmail.com>
2023-12-10 20:03:17 +0000

    final

--- CS_627_IP4.cpp
+++ CS_627_IP4.cpp
@@ -4,53 +4,113 @@ December 9, 2023
 CS 627 IP4
 */
 
-function calculateDifference(X, Y, thresh):
-    I = length(X)  // Number of rows in X
-    J = length(X[0])  // Number of columns in X
-    K = length(Y[0])  // Number of columns in Y
+/*Part I:
+----------------------------------------------------------------------
+For my porfolio I created a deployed website that includes information
+about my skillsets as a developer, experience, and work completed in
+this class. The website is publicly accessible and can be found at:
 
-    // Initialize D matrix
-    D = createMatrix(I, K)
+https://drtaylor93.github.io
+
+The website was created using vanilla javascript, html, and css, and
+while not quite as polished as I would like, I am proud to at least 
+have a starting point as I have wanted to have a personal portfolio
+for quite some time. At the bottom of the page there is a link to my
+github repo where I have listed all of my IP submissions thus far.
+
+*/
+
+
+/*Part II:
+----------------------------------------------------------------------
+For this problem I likened it to signal processing where each bit
+represents a pixel on a screen, but in this case the options are only
+black and white instead of the standard red, blue, or green. While I
+attempted to make the code as close to C++ as possible, it is still
+psudocode code and is not production ready.
+
+
+compareImages: takes 2 images and determines likeness based on a numberic
+               threshhold.
+
+Parameters:
+x vector<vector>: image one to be compared
+y vector<vector>: image two to be compared
+thresh int: determines if the two images are alike or different
+
+Return: 
+String stating whether the images are alike or different
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Function to compare two black-and-white images
+string compareImages(const vector<vector<int>>& x, const vector<vector<int>>& y, int thresh) {
+    int totalDifference = 0;
+
+    // Determining the dimensions of the image being passed in
+    int rows = x.size();
+    int xCols = x[0].size();
+    int yCols = y[0].size();
     
-    // Initialize totalDifference
-    totalDifference = 0
 
-    // Populate D matrix using dynamic programming
-    for i from 1 to I:
-        for j from 1 to K:
-            // Calculate the cost for matching or mismatching pixels
-            cost = abs(X[i-1][j-1] - Y[i-1][j-1])
-            
-            // Recurrence relation for D matrix
-            D[i][j] = cost + min(D[i-1][j], D[i][j-1], D[i-1][j-1])
-    
-    // Find the minimum value in the bottom row of D matrix
-    minVali = min(D[I])
-    
-    // Aggregate the differences
-    totalDifference = totalDifference + minVali
+     // Initialize a new row to store differences (1 if different, 0 if the same)
+    vector<int> differences(xCols, 0);
 
-    // Check if total difference is above the threshold
-    if totalDifference > thresh:
-        return "The images are different"
-    else:
-        return "The images are similar"
+    for (int i = 0; i < rows; ++i) {
+        int currentRowDiff = x[i][0];
 
-function createMatrix(rows, cols):
-    // Helper function to create a 2D matrix with zeros
-    matrix = new 2D array with dimensions rows x cols, initialized with zeros
-    return matrix
+        for (int j = 1; j < min(xCols, yCols); ++j) {
+            int cost = abs(x[i][j] - y[i][j]);
+            currentRowDiff = min(currentRowDiff + cost, x[i][j]);
 
+             // Here we are storing the result of if the two pixels are alike
+             // or different in a new row. If they are the same the value is 0,
+             // otherwise 1.  
+            differences[j] = (x[i][j] != y[i][j]) ? 1 : 0;
+        }
 
+        totalDifference += currentRowDiff;
+    }
+
+    // Check if total difference is above the threshold. If so,
+    // the images are different. I decided to make the threshold
+    // inclusive so a differce of two pixel still classifies as 
+    // similar. 
+    if (totalDifference == 0) {
+        return "The images are the same";
+    } else if(totalDifference < thresh){
+        return "The images are similar to each other";
+    } else {
+        return "The images are different from each other";
+    }
+}
+
+int main() {
+    // Creating two sample rows to compare
+    vector<vector<int>> firstImage = {{1, 1, 0, 1, 0}};
+    vector<vector<int>> secondImage = {{0, 1, 1, 1, 0}};
+
+    int threshold = 2;
+    string result = compareImages(firstImage, secondImage, threshold);
+    cout << result << endl;
+
+    return 0;
+}
 
 /*
 Part 2B:
 ---------------------------------------------------
 The time complexity of the above solution is
-O(m * n). This is because we are using a nested for
+O(x * y). This is because we are using a nested for
 loop to complete the anaylsis of the 2D array, the 
 outer loop to traverse the columns and the inner
 loop to iterate through the rows. The space complexity is
-the same O(m * n) because temporary storage is created to save the
-matrix to memory. 
+the same O(x * y) because temporary storage is created to save the
+difference matrix to memory. 
 */
--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # algorithm-portfolio
-CS 627 Portfolio
+**CS 627 Portfolio**
 
 Within this portfolio you will find a compilation of work from this class demonstrating our coverage of algorithms with a focus on time and space complexity. An outline of each project is detailed below.
 
-Individual Project 1
- Build an in-place, order reversal algorithm. This algorithm will take as an input an array of ints and will reverse the order of the elements in the array, in place (essentially using only the memory in the array). For example, if the array contains five elements [1,2,3,4,5], the output of the algorithm will be [5,4,3,2,1]. 
+**Individual Project 1**
+ 
+Build an in-place, order reversal algorithm. This algorithm will take as an input an array of ints and will reverse the order of the elements in the array, in place (essentially using only the memory in the array). For example, if the array contains five elements [1,2,3,4,5], the output of the algorithm will be [5,4,3,2,1]. 
 
-Individual Project 2
+**Individual Project 2**
+
 Code a search algorithm that searches a list of strings for a particular song. The searching algorithm will have two inputs: the playlist, which is a string array that contains a list of songs in alphabetical order; and a particular song, which is a string. If the song is found in the list, the algorithm will return the index of the song, and it will return -1 otherwise.
 This searching algorithm will employ a divide-and-conquer approach similar to that in binary search, but with a slight variation. In binary search, a list is split in 2 sublists during each step; however, for your assignment, you will build and algorithm that splits the list into 3 sublists during each step.
 
-Individual Project 3
+**Individual Project 3**
+
 You plan to design a greedy algorithm to efficiently transfer media to storage devices. Note that this is an optimization problem. Optimization problems have a general structure and consist of some quantity to be maximized or minimized under some list of constraints. In this problem, you have n files (f1, ..., fn) with corresponding sizes (in MBs) s1, ... sn. Your goal is to store these files onto m disks, d1, ..., dm, that have corresponding storages amounts t1, ..., tm. Note that one file cannot be spread across multiple disks. In this problem, the goal is to minimize the amount of storage that is not used on each disk (that is used). This should also minimize the total number of number of disks being used. That is, you would like to fill up each disk as much as possible while leaving a minimally small amount of unused storage. (In the perfect case, each disk would be perfectly filled, and there would be no unused storage.) If there are any disks left unused, you will be able to return them for a refund.
 
-Individual Project 4
-Compare one black-and-white image into another black-and-white image. There are a number of methods that can be used to perform this task, but your group has agreed that using dynamic programming is a fast and elegant scheme to solve this problem.
+**Individual Project 4**
 
-Assignment
+Compare one black-and-white image into another black-and-white image. There are a number of methods that can be used to perform this task, but your group has agreed that using dynamic programming is a fast and elegant scheme to solve this problem.
 
 Design an algorithm (using pseudocode) that takes in as an input, two 2-D int arrays that are assumed to be 2 black-and-white images: initialImage x, whose dimensions are IxJ, and finalImage y, whose dimensions are IxK. The algorithm will compare x to the y, row-by-row, as defined below. Your algorithm will employ a dynamic programming scheme to compare X to Y identifying the minimal difference between each row.
 
